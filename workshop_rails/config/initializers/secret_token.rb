061a8a977c58f08cd5a7f2ffe6186eda2c43b2c7
Justin Searls <searls@gmail.com>
2012-09-18 07:57:44 -0600

    initial commit

---
+++ workshop_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workshop::Application.config.secret_token = 'e07ab64ee060cd50dc126a411da81a5aa67164604a2e1c3775a7b557ebf43d8343f0dba5216a35f43103e7d4bb71706db26eaccdbe97b61fc05565a4b9e24b95'
